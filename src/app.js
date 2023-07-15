a094ed9d99b9039adfc7f887d5e5bbbc04624819
Argha Chandra Das <arghad@capitalnumbers.com>
2023-07-15 08:11:28 +0530

    added logger error handling and route and controller

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
+import routes from "./routes/index.js";
 
 dotenv.config();
 
@@ -45,8 +47,26 @@ app.use(
 // cors
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Yo");
+// api v1 routes
+app.use("/api/v1", routes);
+
+// app.get("/", (req, res) => {
+//   throw createHttpError.BadRequest("this request is not allowed");
+// });
+
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist"));
+});
+
+// error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import app from "./app.js";
+import logger from "./config/logger.js";
 
 // env variables
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+let server = app.listen(PORT, () => {
+  logger.info(`Server is listening on ${PORT}`);
+});
+
+// handle server errors
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
