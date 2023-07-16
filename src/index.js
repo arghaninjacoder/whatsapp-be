6b3103b23694c65eea52294fb6030084061ebaf8
Argha Chandra Das <arghad@capitalnumbers.com>
2023-07-16 16:40:46 +0530

    Connected mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 import logger from "./config/logger.js";
 
 // env variables
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV);
+
+// Exit on Mongodb error
+mongoose.connection.on("error", (err) => {
+  logger.error("Mongodb connection error: " + err);
+  process.exit(1);
+});
+
+// mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+// mongodb connect
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to mongodb");
+  });
 
 let server = app.listen(PORT, () => {
   logger.info(`Server is listening on ${PORT}`);
